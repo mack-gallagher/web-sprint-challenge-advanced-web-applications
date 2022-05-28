ff040975e202bf190b568ccbae699844b54a5a1e
Mack Gallagher <wingsonthebus@gmail.com>
2022-05-28 00:22:28 -0500

    Meet MVP

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -1,5 +1,25 @@
+import Spinner from './Spinner';
+import React from 'react';
+import { render, fireEvent, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+
+
 // Import the Spinner component into this file and test
 // that it renders what it should for the different props it can take.
 test('sanity', () => {
-  expect(true).toBe(false)
+  expect(true).toBe(true)
+})
+
+test('renders without errors', () => {
+  const wrapper = render(<Spinner on={false} />);
+})
+
+test('when spinner is off, it does not show up', () => {
+  const wrapper = render(<Spinner on={false} />);
+  expect(wrapper.queryByText(/Please wait/i)).not.toBeInTheDocument();
+})
+
+test('when spinner is on, it does show up', () => {
+  const wrapper = render(<Spinner on={true} />);
+  expect(wrapper.queryByText(/Please wait/i)).toBeInTheDocument();
 })
